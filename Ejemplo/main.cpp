917210c9242d0832955dfcc6eab163b46f73364a
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-02 16:45:32 -0600

    Tarea: Codifica la clase Fraccion para este programa

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,58 +1,31 @@
 #include <iostream>
 #include <stdlib.h>
-#include "Punto.h"
-#include "Complejo.h"
+#include "Fraccion.h"
 using namespace std;
 
 int main(void){
-    Punto P1, P2, P3, P4;
-    double d, m;
+    Fraccion A, B, C;
 
     //1.Pide dato(s)
-    cout<<endl<<"Ingresa las coordenadas de P1"<<endl;
-    P1.pideleAlUsuarioTusAtributos();
-    cout<<endl<<"Ingresa las coordenadas de P2"<<endl;
-    P2.pideleAlUsuarioTusAtributos();
+    cout<<endl<<"Ingresa la Fraccion 1"<<endl;
+    A.pideleAlUsuarioTusAtributos();
+    cout<<endl<<"Ingresa la Fraccion 2"<<endl;
+    B.pideleAlUsuarioTusAtributos();
 
-    P3 = sumaPuntos(P1,P2);
-    P4 = restaPuntos(P1,P2);
-    d = calculaDistanciaEntre(P1,P2);
-    m = calculaPendienteDados(P1,P2);
+    C.modificaTuAtributoNumerador(
+        (A.retornaTuAtributoNumerador() * B.retornaTuAtributoDenominador() )
+        +
+        (B.retornaTuAtributoNumerador() * A.retornaTuAtributoDenominador() )
+    );
+    C.modificaTuAtributoDenominador(
+        A.retornaTuAtributoDenominador() * B.retornaTuAtributoDenominador()
+    );
 
     //3.Muestra resultado(s)
     cout<<endl;
-    cout<<"P1 = ";P1.muestraTusAtributos();cout<<endl;
-    cout<<"P2 = ";P2.muestraTusAtributos();cout<<endl;
-    cout<<"P3 = ";P3.muestraTusAtributos();cout<<endl;
-    cout<<"P4 = ";P4.muestraTusAtributos();cout<<endl;
-    cout<<"d = "<<d<<endl;
-    cout<<"m = "<<m<<endl<<endl;
-    system("pause");
-    system("cls");
-
-
-    Complejo C1, C2, C3, C4, C5, C6;
-
-    //1.Pide dato(s)
-    cout<<endl<<"Ingresa C1"<<endl;
-    C1.pideleAlUsuarioTusAtributos();
-    cout<<endl<<"Ingresa C2"<<endl;
-    C2.pideleAlUsuarioTusAtributos();
-
-	C3 = sumaComplejos(C1,C2);
-	C4 = restaComplejos(C1,C2);
-	C5 = multiplicaComplejos(C1,C2);
-	C6 = divideComplejos(C1,C2);
-
-    //3.Muestra resultado(s)
-    cout<<endl;
-    cout<<"C1 = ";C1.muestraTusAtributos();cout<<endl;
-    cout<<"C2 = ";C2.muestraTusAtributos();cout<<endl;
-    cout<<"C3 = ";C3.muestraTusAtributos();cout<<endl;
-    cout<<"C4 = ";C4.muestraTusAtributos();cout<<endl;
-    cout<<"C5 = ";C5.muestraTusAtributos();cout<<endl;
-    cout<<"C6 = ";C6.muestraTusAtributos();cout<<endl;
+    cout<<"A   = ";A.muestraTusAtributos();cout<<endl;
+    cout<<"B   = ";B.muestraTusAtributos();cout<<endl;
+    cout<<"A+B = ";C.muestraTusAtributos();cout<<endl;
 
     return 0;
 }
-
