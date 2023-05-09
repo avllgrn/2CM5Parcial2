1f5aaf1d25396abc395eed631b54fec170541f20
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-08 18:13:57 -0600

    Prueba para clase Circulo

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <stdlib.h>
-#include "Fraccion.h"
+#include "Circulo.h"
 using namespace std;
 
 int main(void){
-    Fraccion A(1,2), B, C, D, E, F;
-    B.modificaTusAtributos(3,4);
+    Circulo A, B(1), C, D;
 
-    C = sumaFracciones(A,B);
-    D = restaFracciones(A,B);
-    E = multiplicaFracciones(A,B);
-    F = divideFracciones(A,B);
+    cout<<"Ingresa C"<<endl;
+    C.pideleAlUsuarioTusDatos();
+
+    D.modificaTuRadio(3 * C.dameTuRadio());
 
     //3.Muestra resultado(s)
     cout<<endl;
-    cout<<"A   = ";A.muestraTusAtributos();cout<<endl;
-    cout<<"B   = ";B.muestraTusAtributos();cout<<endl;
-    cout<<"A+B = ";C.muestraTusAtributos();cout<<endl;
-    cout<<"A-B = ";D.muestraTusAtributos();cout<<endl;
-    cout<<"A*B = ";E.muestraTusAtributos();cout<<endl;
-    cout<<"A/B = ";F.muestraTusAtributos();cout<<endl;
-    cout<<endl;
 
+    cout<<"A\n"<<"Radio:\t   "<<A.dameTuRadio()<<endl
+        <<"Diametro:  "<<A.dameTuDiametro()<<endl
+        <<"Area:\t   "<<A.dameTuArea()<<endl
+        <<"Perimetro: "<<A.dameTuPerimetro()<<endl;
+
+    cout<<"B\n";B.muestraTusDatos();cout<<endl;
+    cout<<"C\n";C.muestraTusDatos();cout<<endl;
+    cout<<"D\n";D.muestraTusDatos();cout<<endl;
+
+    cout<<endl;
     return 0;
 }
