bddb9caae7d40fd1e0a0a9bbf1916e12ea59b96b
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-22 22:43:41 -0600

    Pruebas para clase Hora

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,63 +1,64 @@
 #include <iostream>
 #include <stdlib.h>
-#include "Fecha.h"
+#include "Hora.h"
 using namespace std;
 
 int main(void){
-    Fecha F1;
-    Fecha F2(22,5,2023);
+    Hora H1;
+    Hora H2(11,12,13);
 
     cout<<"Pruebas de muestraTusDatos() y constructores"<<endl<<endl;
-    cout<<"F1"<<endl;
-    F1.muestraTusDatos();
+    cout<<"H1"<<endl;
+    H1.muestraTusDatos();
     cout<<endl<<endl;
-    cout<<"F2"<<endl;
-    F2.muestraTusDatos();
+    cout<<"H2"<<endl;
+    H2.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
     system("cls");
 
-    cout<<"Pruebas de modificaTuDia(), modificaTuMes(), modificaTuAnio()"<<endl
-        <<"dameTuDia(), dameTuMes() y dameTuAnio()"<<endl<<endl;
-    cout<<"F1"<<endl;
-    F1.muestraTusDatos();
+    cout<<"Pruebas de modificaTuHora(), modificaTuMinuto(), modificaTuSegundo()"<<endl
+        <<"dameTuHora(), dameTuMinuto() y dameTuSegundo()"<<endl<<endl;
+    cout<<"H1"<<endl;
+    H1.muestraTusDatos();
     cout<<endl<<endl;
-    cout<<"F2"<<endl;
-    F2.muestraTusDatos();
+    cout<<"H2"<<endl;
+    H2.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
-    F1.modificaTuDia(F2.dameTuDia());
-    F1.modificaTuMes(F2.dameTuMes());
-    F1.modificaTuAnio(F2.dameTuAnio());
-    cout<<"F1"<<endl;
-    F1.muestraTusDatos();
+    H1.modificaTuHora(H2.dameTuHora());
+    H1.modificaTuMinuto(H2.dameTuMinuto());
+    H1.modificaTuSegundo(H2.dameTuSegundo());
+    cout<<"H1"<<endl;
+    H1.muestraTusDatos();
     cout<<endl<<endl;
-    cout<<"F2"<<endl;
-    F2.muestraTusDatos();
+    cout<<"H2"<<endl;
+    H2.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
     system("cls");
 
     cout<<"Pruebas de modificaTusDatos()"<<endl<<endl;
-    cout<<"F1"<<endl;
-    F1.muestraTusDatos();
+    cout<<"H1"<<endl;
+    H1.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
-    F1.modificaTusDatos(15,9,1820);
-    cout<<"F1"<<endl;
-    F1.muestraTusDatos();
+    H1.modificaTusDatos(14,15,16);
+    cout<<"H1"<<endl;
+    H1.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
     system("cls");
 
     cout<<"Pruebas de pideleAlUsuarioTusDatos()"<<endl<<endl;
-    cout<<"F1"<<endl;
-    F1.muestraTusDatos();
+    cout<<"H1"<<endl;
+    H1.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
-    F1.pideleAlUsuarioTusDatos();
-    cout<<endl<<"F1"<<endl;
-    F1.muestraTusDatos();
+    cout<<endl<<endl;
+    H1.pideleAlUsuarioTusDatos();
+    cout<<endl<<"H1"<<endl;
+    H1.muestraTusDatos();
     cout<<endl<<endl;
 
     return 0;
