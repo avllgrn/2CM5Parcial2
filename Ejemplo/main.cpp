ceca9919da371554ec95cee71399b7f72062df86
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-22 21:38:55 -0600

    Pruebas para clase CirculoP

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,32 +1,99 @@
 #include <iostream>
 #include <stdlib.h>
-#include "Rectangulo.h"
+#include "CirculoP.h"
 using namespace std;
 
 int main(void){
-    Rectangulo A, B, C(4,5), D, E;
+    Punto A(1,2), B(3,4);
+    CirculoP Q1;
+    CirculoP Q2(A,B);
+    CirculoP Q3(5,5,5,6);
 
-    cout<<"Ingresa C"<<endl;
-    B.pideleAlUsuarioTusDatos();
+    cout<<"Pruebas de muestraTusDatos() y constructores"<<endl<<endl;
+    cout<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
+    cout<<"Q2"<<endl;
+    Q2.muestraTusDatos();
+    cout<<endl<<endl;
+    cout<<"Q3"<<endl;
+    Q3.muestraTusDatos();
+    cout<<endl<<endl;
+    system("pause");
+    system("cls");
 
-    D.modificaTuBase(2 * C.dameTuBase());
-    D.modificaTuAltura(2 * C.dameTuAltura());
+    cout<<"Pruebas de modificaTuC(), modificaTuP(), "
+        <<"dameTuC() y dameTuP()"<<endl<<endl;
+    cout<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
+    cout<<"Q2"<<endl;
+    Q2.muestraTusDatos();
+    cout<<endl<<endl;
+    system("pause");
+    Q1.modificaTuC(Q2.dameTuC());
+    Q1.modificaTuP(Q2.dameTuP());
+    cout<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
+    cout<<"Q2"<<endl;
+    Q2.muestraTusDatos();
+    cout<<endl<<endl;
+    system("pause");
+    system("cls");
 
-    E.modificaTusDatos(3 * D.dameTuBase(), 3 * D.dameTuAltura());
+    cout<<"Pruebas de modificaTuC() y modificaTuP()"<<endl<<endl;
+    cout<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
+    system("pause");
+    Q1.modificaTuC(0,0);
+    Q1.modificaTuP(0,0);
+    cout<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
+    system("pause");
+    system("cls");
 
-    //3.Muestra resultado(s)
-    cout<<endl;
+    cout<<"Pruebas de modificaTusDatos()"<<endl<<endl;
+    cout<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
+    cout<<"Q3"<<endl;
+    Q3.muestraTusDatos();
+    cout<<endl<<endl;
+    system("pause");
+    Q1.modificaTusDatos(Q3.dameTuC(),Q3.dameTuP());
+    cout<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
+    cout<<"Q3"<<endl;
+    Q3.muestraTusDatos();
+    cout<<endl<<endl;
+    system("pause");
+    system("cls");
 
-    cout<<"A\n"<<"Base:\t   "<<A.dameTuBase()<<endl
-        <<"Altura:    "<<A.dameTuAltura()<<endl
-        <<"Area:\t   "<<A.dameTuArea()<<endl
-        <<"Perimetro: "<<A.dameTuPerimetro()<<endl;
+    cout<<"Pruebas de modificaTusDatos()"<<endl<<endl;
+    cout<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
+    system("pause");
+    Q1.modificaTusDatos(0,0,0,0);
+    cout<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
+    system("pause");
+    system("cls");
 
-    cout<<"B\n";B.muestraTusDatos();cout<<endl;
-    cout<<"C\n";C.muestraTusDatos();cout<<endl;
-    cout<<"D\n";D.muestraTusDatos();cout<<endl;
-    cout<<"E\n";E.muestraTusDatos();cout<<endl;
+    cout<<"Pruebas de pideleAlUsuarioTusDatos()"<<endl<<endl;
+    cout<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
+    system("pause");
+    Q1.pideleAlUsuarioTusDatos();
+    cout<<endl<<"Q1"<<endl;
+    Q1.muestraTusDatos();
+    cout<<endl<<endl;
 
-    cout<<endl;
     return 0;
 }
