6f9bf908313cc8d5f3dea72974cd2f8930cd2a41
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-22 22:47:39 -0600

    Prototipos de clases Fecha y Hora

---
+++ miBiblioteca/Fecha.h
@@ -0,0 +1,29 @@
+#ifndef FECHA_H
+#define FECHA_H
+
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Fecha{
+private:
+    int dia;
+    int mes;
+    int anio;
+public:
+	Fecha(void);
+	Fecha(int dia, int mes, int anio);
+	~Fecha(void);
+	void pideleAlUsuarioTusDatos(void);
+	void muestraTusDatos(void);
+	int dameTuDia(void);
+	void modificaTuDia(int dia);
+	int dameTuMes(void);
+	void modificaTuMes(int mes);
+	int dameTuAnio(void);
+	void modificaTuAnio(int Anio);
+	void modificaTusDatos(int dia, int mes, int anio);
+};
+
+#endif // FECHA_H
---
+++ miBiblioteca/Hora.h
@@ -0,0 +1,30 @@
+#ifndef HORA_H
+#define HORA_H
+
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Hora{
+private:
+    int hora;
+    int minuto;
+    int segundo;
+public:
+	Hora(void);
+	Hora(int hora, int minuto, int anio);
+	~Hora(void);
+	void pideleAlUsuarioTusDatos(void);
+	void muestraTusDatos(void);
+	int dameTuHora(void);
+	void modificaTuHora(int hora);
+	int dameTuMinuto(void);
+	void modificaTuMinuto(int minuto);
+	int dameTuSegundo(void);
+	void modificaTuSegundo(int segundo);
+	void modificaTusDatos(int hora, int minuto, int segundo);
+};
+
+
+#endif // HORA_H
