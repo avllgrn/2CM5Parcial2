30f073dcb1b515bcb3acad16fb79d7d5d294bffd
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-22 22:38:15 -0600

    Pruebas para clase Fecha

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,98 +1,63 @@
 #include <iostream>
 #include <stdlib.h>
-#include "CirculoP.h"
+#include "Fecha.h"
 using namespace std;
 
 int main(void){
-    Punto A(1,2), B(3,4);
-    CirculoP Q1;
-    CirculoP Q2(A,B);
-    CirculoP Q3(5,5,5,6);
+    Fecha F1;
+    Fecha F2(22,5,2023);
 
     cout<<"Pruebas de muestraTusDatos() y constructores"<<endl<<endl;
-    cout<<"Q1"<<endl;
-    Q1.muestraTusDatos();
+    cout<<"F1"<<endl;
+    F1.muestraTusDatos();
     cout<<endl<<endl;
-    cout<<"Q2"<<endl;
-    Q2.muestraTusDatos();
-    cout<<endl<<endl;
-    cout<<"Q3"<<endl;
-    Q3.muestraTusDatos();
+    cout<<"F2"<<endl;
+    F2.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
     system("cls");
 
-    cout<<"Pruebas de modificaTuC(), modificaTuP(), "
-        <<"dameTuC() y dameTuP()"<<endl<<endl;
-    cout<<"Q1"<<endl;
-    Q1.muestraTusDatos();
+    cout<<"Pruebas de modificaTuDia(), modificaTuMes(), modificaTuAnio()"<<endl
+        <<"dameTuDia(), dameTuMes() y dameTuAnio()"<<endl<<endl;
+    cout<<"F1"<<endl;
+    F1.muestraTusDatos();
     cout<<endl<<endl;
-    cout<<"Q2"<<endl;
-    Q2.muestraTusDatos();
-    cout<<endl<<endl;
-    system("pause");
-    Q1.modificaTuC(Q2.dameTuC());
-    Q1.modificaTuP(Q2.dameTuP());
-    cout<<"Q1"<<endl;
-    Q1.muestraTusDatos();
-    cout<<endl<<endl;
-    cout<<"Q2"<<endl;
-    Q2.muestraTusDatos();
+    cout<<"F2"<<endl;
+    F2.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
-    system("cls");
-
-    cout<<"Pruebas de modificaTuC() y modificaTuP()"<<endl<<endl;
-    cout<<"Q1"<<endl;
-    Q1.muestraTusDatos();
+    F1.modificaTuDia(F2.dameTuDia());
+    F1.modificaTuMes(F2.dameTuMes());
+    F1.modificaTuAnio(F2.dameTuAnio());
+    cout<<"F1"<<endl;
+    F1.muestraTusDatos();
     cout<<endl<<endl;
-    system("pause");
-    Q1.modificaTuC(0,0);
-    Q1.modificaTuP(0,0);
-    cout<<"Q1"<<endl;
-    Q1.muestraTusDatos();
+    cout<<"F2"<<endl;
+    F2.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
     system("cls");
 
     cout<<"Pruebas de modificaTusDatos()"<<endl<<endl;
-    cout<<"Q1"<<endl;
-    Q1.muestraTusDatos();
-    cout<<endl<<endl;
-    cout<<"Q3"<<endl;
-    Q3.muestraTusDatos();
+    cout<<"F1"<<endl;
+    F1.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
-    Q1.modificaTusDatos(Q3.dameTuC(),Q3.dameTuP());
-    cout<<"Q1"<<endl;
-    Q1.muestraTusDatos();
-    cout<<endl<<endl;
-    cout<<"Q3"<<endl;
-    Q3.muestraTusDatos();
-    cout<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<"Pruebas de modificaTusDatos()"<<endl<<endl;
-    cout<<"Q1"<<endl;
-    Q1.muestraTusDatos();
-    cout<<endl<<endl;
-    system("pause");
-    Q1.modificaTusDatos(0,0,0,0);
-    cout<<"Q1"<<endl;
-    Q1.muestraTusDatos();
+    F1.modificaTusDatos(15,9,1820);
+    cout<<"F1"<<endl;
+    F1.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
     system("cls");
 
     cout<<"Pruebas de pideleAlUsuarioTusDatos()"<<endl<<endl;
-    cout<<"Q1"<<endl;
-    Q1.muestraTusDatos();
+    cout<<"F1"<<endl;
+    F1.muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
-    Q1.pideleAlUsuarioTusDatos();
-    cout<<endl<<"Q1"<<endl;
-    Q1.muestraTusDatos();
+    F1.pideleAlUsuarioTusDatos();
+    cout<<endl<<"F1"<<endl;
+    F1.muestraTusDatos();
     cout<<endl<<endl;
 
     return 0;
